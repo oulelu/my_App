48a2b2612b4dadea333bef09b91fd134a2df0a7b
oulelu <oulelu@globalbanking.ac.uk>
2026-04-27 14:08:51 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import calendar
+
+# To take month and year input from the user
+year = int(input("Enter year: "))
+month = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(year, month))
